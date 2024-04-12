1784b4a495ed17078ac41ed104b51186775569c0
Alexandre <zenialexandre123@gmail.com>
2024-04-11 21:29:11 -0300

    feat: adding CG_BIBLIOTECA and first exercise first archives

---
+++ Unidade2/CG_N2_1/Circulo.cs
@@ -0,0 +1,11 @@
+using CG_Biblioteca;
+
+namespace gcgcg
+{
+    internal class Circulo : Objeto
+    {
+        public Circulo(Objeto _paiRef, ref char _rotulo, Ponto4D centralPoint, float radius) : base(_paiRef, ref _rotulo, objetoFilho) {
+            //..
+        }
+    }
+}
